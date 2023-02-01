b8069cc865980200b1807fb1be737bec7ea49b8a
Rlasserre <r_lasserre@hotmail.com>
2023-01-31 21:40:41 -0300

    Update

--- main.go
+++ main.go
@@ -11,6 +11,14 @@ type currentAccounts struct {
 
 func main() {
 
-	fmt.Println(currentAccounts{})
+	rafaelAccount := currentAccounts{
+		accountOwner:   "Rafael",
+		bankBranch:     589,
+		accountNumber:  123456,
+		accountBalance: 125.58}
+
+	maraisaAccount := currentAccounts{"Maraisa", 756, 369852, 287.32}
+
+	fmt.Println(rafaelAccount, maraisaAccount)
 
 }
