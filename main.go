9bf038d0db894af5ea7a336b04de4fbe782e495e
Rlasserre <r_lasserre@hotmail.com>
2023-01-31 21:30:03 -0300

    update

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import "fmt"
+
+type currentAccounts struct {
+	accountOwner   string
+	bankBranch     int
+	accountNumber  int
+	accountBalance float64
+}
+
 func main() {
+
+	fmt.Println(currentAccounts{})
 
 }
