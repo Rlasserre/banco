f61b0e9ff4e82db83894536e9709c3fe30c3caac
Rlasserre <r_lasserre@hotmail.com>
2023-02-06 10:22:39 -0300

    update

--- main.go
+++ main.go
@@ -9,26 +9,27 @@ type currentAccounts struct {
 	accountBalance float64
 }
 
+func (c *currentAccounts) withdrawn(withdrawnAmount float64) string {
+
+	authorized := withdrawnAmount > 0 && withdrawnAmount <= c.accountBalance
+
+	if authorized {
+		c.accountBalance -= withdrawnAmount
+		return "Saque realizado com sucesso"
+	} else {
+		return "Saldo insuficiente"
+	}
+}
+
 func main() {
 
-	rafaelAccount := currentAccounts{
-		accountOwner:   "Rafael",
-		bankBranch:     589,
-		accountNumber:  123456,
-		accountBalance: 125.58}
+	silviaAccounts := currentAccounts{}
+	silviaAccounts.accountOwner = "Silvia"
+	silviaAccounts.accountBalance = 500
 
-	rafaelAccount2 := currentAccounts{
-		accountOwner:   "Rafael",
-		bankBranch:     589,
-		accountNumber:  123456,
-		accountBalance: 125.58}
+	fmt.Println(silviaAccounts.accountBalance)
 
-	fmt.Println(rafaelAccount == rafaelAccount2)
-	// maraisaAccount := currentAccounts{"Maraisa", 756, 369852, 287.32}
+	fmt.Println(silviaAccounts.withdrawn(600))
 
-	// var chrisAccount *currentAccounts
-	// chrisAccount = new(currentAccounts)
-	// chrisAccount.accountOwner = "Chris"
-
-	// fmt.Println(rafaelAccount, maraisaAccount, chrisAccount)
+	fmt.Println(silviaAccounts.accountBalance)
 }
