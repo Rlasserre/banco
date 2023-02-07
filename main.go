2bbc64856a16364f07e71d8b46995362c8071fee
Rlasserre <r_lasserre@hotmail.com>
2023-02-07 15:25:38 -0300

    update

--- main.go
+++ main.go
@@ -8,22 +8,15 @@ import (
 
 func main() {
 
-	clientSilvia := bankclients.AccountOwner{"Silvia", "123.456.789-00", "Professora"}
-	silviaAccount := accounts.CheckingAccounts{clientSilvia, 123, 123456, 1000}
+	clientSilvia := bankclients.AccountOwner{Name: "Silvia", CPF: "123.456.789-00", Ocupation: "Professora"}
+	silviaAccounts := accounts.CheckingAccounts{AccountOwner: clientSilvia, BankBranch: 123, AccountNumber: 123456, AccountBalance: 1000}
 
-	ricardoAccounts := accounts.CheckingAccounts{
-		AccountOwner: bankclients.AccountOwner{
-			Name:      "Ricardo",
-			CPF:       "321.654.987-11",
-			Ocupation: "Motorista",
-		},
-		BankBranch:     123,
-		AccountNumber:  789654,
-		AccountBalance: 1000}
+	clientRicardo := bankclients.AccountOwner{Name: "Ricardo", CPF: "321.654.987.23", Ocupation: "Motorista"}
+	ricardoAccounts := accounts.CheckingAccounts{AccountOwner: clientRicardo, BankBranch: 456, AccountNumber: 789561, AccountBalance: 875}
 
 	status := silviaAccounts.Transfer(200, &ricardoAccounts)
 
 	fmt.Println(status)
-	fmt.Println(silviaAccount)
+	fmt.Println(silviaAccounts)
 	fmt.Println(ricardoAccounts)
 }
